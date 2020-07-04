e415f14bbb0ed09516eb233f442e8ff38e06508a
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-04 11:21:31 -0700

    RESTful app

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const blogSchema = new mongoose.Schema({
     body: String,
     created: {type: Date, default: Date.now}
 });
-const Blog = mongoose.model('blogSchema');
+const Blog = mongoose.model('blogSchema', blogSchema);
 
 // blog.create({
 //     title: 'Test post',
