4a642f6d7bc379849d170d57646b4b9722302297
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-04 14:43:13 -0700

    RESTful app

--- app.js
+++ app.js
@@ -61,6 +61,22 @@ app.get('/blogs/new', function(req, res) {
     res.render('new');
 });
 
+/* new POST route */
+app.post('/blogs', function(req, res) {
+    // create blog
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render('new');
+        }
+        else {
+            res.redirect('/blogs');
+        }
+    });
+    // redirect
+});
+
+
+
 // start server
 const port = 1776;
 app.listen(port, console.log(`Server now listening on port ${port}`));
