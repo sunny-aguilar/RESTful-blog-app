544dadb29eaa40a5d160560aac28d5c96d17f6b1
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-04 10:55:33 -0700

    RESTful app

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // RESTful Web App
 
+// npm modules
 const bodyParser    = require('body-parser'),
 mongoose            = require('mongoose'),
 express             = require('express'),
 app                 = express();
 
+
+// connect to MongoDB
+mongoose.connect('mongodb://localhost/restful_blog_app', {
+    //
+});
 
 // map routes
 app.get('/', function(req, res) {
