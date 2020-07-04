b6c01ca882f502ef4ef11ff2b436f133df0f8483
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-04 10:47:26 -0700

    RESTful app

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require('express');
 const app = express();
 
 
+// map routes
+app.get('/', function(req, res) {
+    res.send('RESTful Web App');
+});
 
 
 
-
-
+// start server
+const port = 1776;
+app.listen(port, console.log(`Server now listening on port ${port}`));
 
 
 
